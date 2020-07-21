33709684f99037f6c76c852e0d5f869da8470101
geumilbae <geumil.bae@projectybl.com>
2020-07-21 13:56:53 +0900

    Initial commit

---
+++ hello-react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <h1>Hi, React!!</h1>
+            <h2>Are you alright?</h2>
+        </>
+  );
+}
+
+export default App;
