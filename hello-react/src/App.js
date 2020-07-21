a1c394fe4277f668ae9d798ebe153af2c3636726
geumilbae <geumil.bae@projectybl.com>
2020-07-21 22:06:08 +0900

    example of App.css

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    const name = undefined;
-    return (<div>{name || 'React'}</div>);
+    const name = "React";
+    return <div className="react">{name}</div>;
 }
 
 export default App;
