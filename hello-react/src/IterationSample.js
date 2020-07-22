52cb166802995c841cbac6e4dbe34c080159753d
geumilbae <geumil.bae@projectybl.com>
2020-07-22 16:24:41 +0900

    6.4 application: add function add

--- hello-react/src/IterationSample.js
+++ hello-react/src/IterationSample.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 
 const IterationSample = () => {
-    const names = ['Snowman', 'Ice', 'Snow', 'Wind'];
-    const nameList = names.map((name, index) => <li key={index}>{name}</li>);
-    return <ul>{nameList}</ul>;
+    const [names, setNames] = useState([
+        {id: 1, text: 'Snowman'},
+        {id: 2, text: 'Ice'},
+        {id: 3, text: 'Snow'},
+        {id: 4, text: 'Wind'}
+    ]);
+    const [inputText, setInputText] = useState('');
+    const [nextId, setNextId] = useState(5);  // 새로운 항목을 추가할 때 사용할 id
+
+    const onChange = e => setInputText(e.target.value);
+    const onClick = () => {
+        const nextNames = names.concat({
+            id: nextId,
+            text: inputText
+        });
+        setNextId(nextId + 1);
+        setNames(nextNames);
+        setInputText('');
+    }
+
+    const namesList = names.map(name => <li key={name.id}>{name.text}</li>);
+
+    return (
+        <>
+            <input value={inputText} onChange={onChange}/>
+            <button onClick={onClick}>Add</button>
+            <ul>{namesList}</ul>
+        </>
+    );
 };
 
 export default IterationSample;
+
