0f12fd62ad2dcf7570c1342946af1c1eeeac3189
geumilbae <geumil.bae@projectybl.com>
2020-07-22 10:10:44 +0900

    3.4. state: multiple state parameters

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -5,14 +5,16 @@ class Counter extends Component {
         super(props);
         // set initial state
         this.state = {
-            number: 0
+            number: 0,
+            fixedNumber: 0
         };
     }
     render() {
-        const {number} = this.state;  // use this.state to lookup state
+        const {number, fixedNumber} = this.state;  // use this.state to lookup state
         return (
             <div>
                 <h1>{number}</h1>
+                <h2>Fixed number: {fixedNumber}</h2>
                 {/* set function to call when clicking button by onclick */}
                 {/* able to set new value of state through this.setState */}
                 <button
